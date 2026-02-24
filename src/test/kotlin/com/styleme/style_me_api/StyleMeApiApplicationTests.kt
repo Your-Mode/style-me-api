e947b4530cea0ebb0bae3e87e04ffe031b8c165e
Tnalxmsk <alstnwkd990@naver.com>
2026-02-24 11:39:29 +0900

    init: setup project

---
+++ src/test/kotlin/com/styleme/style_me_api/StyleMeApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.styleme.style_me_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StyleMeApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
